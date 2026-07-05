e8d89463df3612a5af890e4b0fdbe72ea50c2830
xheize <yejun1101@naver.com>
2026-07-05 16:52:49 +0900

    refactor: completely remove Authentik OIDC and Auth.js dependencies

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,28 +3,8 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		interface Locals {
-			// Auth.js 세션 정보
-			session?: {
-				user?: {
-					id: string;
-					name?: string;
-					email?: string;
-					image?: string;
-				};
-			};
-		}
-		interface PageData {
-			// 페이지에서 사용할 세션 데이터
-			session?: {
-				user?: {
-					id: string;
-					name?: string;
-					email?: string;
-					image?: string;
-				};
-			};
-		}
+		// interface Locals {}
+		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
